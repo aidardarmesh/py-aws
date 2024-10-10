f0232b00ce6c522d0b12862b26121346984444f2
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-10-10 14:26:39 +0500

    Added spread placement group

--- main.tf
+++ main.tf
@@ -13,9 +13,17 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+# Create a spread placement group (compatible with all instance types)
+resource "aws_placement_group" "app_cluster" {
+  name     = "app-cluster"
+  strategy = "spread"
+}
+
+# Create an EC2 instance and assign it to the placement group
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  placement_group = aws_placement_group.app_cluster.name
 
   tags = {
     Name = "ExampleAppServerInstance"
